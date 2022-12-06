7530f9db9a6065f04f10f17af066bed82ff75916
Sandeep K. Dasari <sandeepkumar25820@gmail.com>
2022-12-06 20:04:25 +0530

    Redux Practice Started.:)

---
+++ MY_PRACTICE_WORK/Redux/Basic-IceCreamStore/index.js
@@ -0,0 +1,12 @@
+// TODO: Ice Cream Application
+
+console.log("This is the Ice Cream Shop Application.");
+
+// TODO: Import the 'redux' from the 'react'
+const redux = require("redux")
+const createStore = redux.createStore;
+
+// TODO: Defining the ACTIONS 
+const ACTIONS = {
+    
+}
