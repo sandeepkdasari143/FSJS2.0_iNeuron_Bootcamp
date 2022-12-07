e8e17baced9d12831f28f586114620c3d3302608
Sandeep K. Dasari <sandeepkumar25820@gmail.com>
2022-12-07 16:01:00 +0530

    This is The first Firebase Application

--- MY_PRACTICE_WORK/Redux/Basic-IceCreamStore/index.js
+++ MY_PRACTICE_WORK/Redux/Basic-IceCreamStore/index.js
@@ -1,5 +1,4 @@
 // TODO: Ice Cream Application
-
 console.log("This is the Ice Cream Shop Application.");
 
 // TODO: Import the 'redux' from the 'react'
@@ -8,5 +7,33 @@ const createStore = redux.createStore;
 
 // TODO: Defining the ACTIONS 
 const ACTIONS = {
-    
+    CAKE_ORDERED : 'CAKE_ORDERED',
 }
+
+//TODO: Specify what action the function should be performed.
+const orderCake = () => {
+    return {
+        type: ACTIONS.CAKE_ORDERED,
+        noOfCakesOrdered: 1,
+    }
+}
+
+// TODO: Initialise the state. 
+const initialState = {
+    noOfCakes: 100,
+}
+
+//TODO: Defining the function which representing the REDUCER.
+const iceCreamReducer = (state = initialState, action) = {
+    switch(action.type){
+        case CAKE_ORDERED:
+            return {
+                ...state,
+                noOfCakes: state.noOfCakes - 1,
+            }
+            break;
+        default: return state;
+    }
+}
+
+// TODO: Creating the Store. 
