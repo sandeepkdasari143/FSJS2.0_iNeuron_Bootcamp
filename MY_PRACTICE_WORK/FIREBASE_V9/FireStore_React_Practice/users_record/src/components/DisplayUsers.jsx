4726685461af7b60982b6146d58181edfa4d66e4
Sandeep K. Dasari <sandeepkumar25820@gmail.com>
2022-12-15 19:43:30 +0530

    Updated

--- MY_PRACTICE_WORK/FIREBASE_V9/FireStore_React_Practice/users_record/src/components/DisplayUsers.jsx
+++ MY_PRACTICE_WORK/FIREBASE_V9/FireStore_React_Practice/users_record/src/components/DisplayUsers.jsx
@@ -1,24 +1,32 @@
-import React, { useEffect } from 'react'
-import { db } from '../config/firebase';
-import { getDocs, collection} from 'firebase/firestore';
+import { collection, onSnapshot} from "firebase/firestore"
+import { useEffect, useState } from "react";
+import { db } from "../config/firebase"
+
 export default function DisplayUsers() {
 
-    const usersColRef = collection(db, 'users');
-    let users = [];
-    useEffect((users) => {
-        getDocs(usersColRef).then((snapshot) => {
-        snapshot.docs.map((doc) => {
-            return users.push({ ...doc.data(), id: doc.id });
+    const [users, setUsers] = useState([]);
+
+    //todo: have a reference to the firestore users collection.
+    const usersColRef = collection(db, "users");
+
+    useEffect(() => {
+        onSnapshot(usersColRef, (snapshot) => {
+            setUsers(snapshot.docs.map((doc) => {
+                return {...doc.data(), id: doc.id}
+            }))
         })
     })
-    }, [usersColRef])
     
     return (
-        <div>
-            {users.map(user => {
-                return <p>{user?.email}</p>
+        <div className="text-white font-bold">
+            {users.map((user) => { 
+                return (
+                    <h1 key={user.id}>{user.email}</h1>
+                )
             })}
-            <button className='bg-white text-black px-5 py-3 rounded-lg '>click me for data</button>
         </div>
     )
+
+
+    
 }
